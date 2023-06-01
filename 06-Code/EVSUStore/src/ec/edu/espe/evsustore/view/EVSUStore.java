cf16b68474adb9cc5f8c51694baf46f56f2c4284
EspinAndres01 <andresboki2000@gmail.com>
2023-06-01 02:36:37 -0500

    Other change git stash

--- 06-Code/EVSUStore/src/ec/edu/espe/evsustore/view/EVSUStore.java
+++ 06-Code/EVSUStore/src/ec/edu/espe/evsustore/view/EVSUStore.java
@@ -10,10 +10,14 @@ public class EVSUStore {
 
     public static void main(String[] args) {
         UserInterface ui = new UserInterface();
+<<<<<<< Updated upstream
         
         
         ui.selecOption();
                 
+=======
+        ui.selecOption();
+>>>>>>> Stashed changes
     }
     
 }
